ca23bb8206eeb9ace9aff0e6e0a4884d8e8f4e33
Ricardo Umana <u3dide@gmail.com>
2024-08-09 17:05:14 -0500

    iniciando Js

---
+++ textEncript.js
@@ -0,0 +1,14 @@
+const textoIngreso = document.querySelector(".texto-ingreso");
+const textoEncriptado = document.querySelector(".texto-encriptado");
+
+/*
+    La letra "e" es convertida para "enter"
+    La letra "i" es convertida para "imes"
+    La letra "a" es convertida para "ai"
+    La letra "o" es convertida para "ober"
+    La letra "u" es convertida para "ufat"
+*/
+
+let arrayDoble = [["a", "ai"],["e", "enter"], ["i", "imes"], ["o", "ober"], ["u", "ufat"]];
+
+console.table(arrayDoble);
