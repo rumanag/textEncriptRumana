41fcd143d0e991cfdd16a0e64fd9dcf8de317c52
Ricardo Umana <u3dide@gmail.com>
2024-08-13 18:52:47 -0500

    terminadas la encriptación y desencriptación

--- textEncript.js
+++ textEncript.js
@@ -1,5 +1,5 @@
-const textoIngreso = document.querySelector(".texto-ingreso");
-const textoEncriptado = document.querySelector(".texto-encriptado");
+const textoIngreso = document.querySelector( " .texto-ingreso");
+const textoEncriptado = document.querySelector(" .texto-encriptado");
 
 /*
     La letra "e" es convertida para "enter"
@@ -9,6 +9,53 @@ const textoEncriptado = document.querySelector(".texto-encriptado");
     La letra "u" es convertida para "ufat"
 */
 
-let arrayDoble = [["a", "ai"],["e", "enter"], ["i", "imes"], ["o", "ober"], ["u", "ufat"]];
+function btnEncriptar(){
 
-console.table(arrayDoble);
+    const textoEncriptar = encriptar(textoIngreso.value); //textoIngreso pasa el contenido a textoEncriptar.
+    textoEncriptado.value = textoEncriptar;
+    textoIngreso.value="";
+    textoEncriptado.style.backgroundImage= "none";
+}
+
+function encriptar(stringEncriptada) {                                              // StringEncriptada tiene el contenido de textAra 
+    
+    let matrizCodigo= [["e", "enter"], ["i","imes"], ["a", "ai",],["o", "ober"], ["u", "ufat"]];
+        stringEncriptada = stringEncriptada.toLowerCase();
+
+    let i=0
+    for(  i=0; i < matrizCodigo.length; i++){
+          
+        if(stringEncriptada.includes(matrizCodigo[i][0])){                          // Revisa si hay caracteres  iguales al contenido en el índice i, posición 0, de la matríz.
+
+
+            stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1]);  // ENCRIPTACIÓN: Reemplaza en el texto todos los
+        }                                                                                           //  caracteres que se encuentran en el índice i, posición 0,
+    }    
+    console.log(`stringEncriptada: ${stringEncriptada}`);                                                                                        // por los caracteres que se encuentran en el índice i, posición 1.
+    return stringEncriptada;                                                        // pasa el valor de retorno a la llamada del  botonEncriptar 
+}
+
+function btnDesencriptar(){
+
+    const textoEncriptar = desencriptar(textoIngreso.value);
+    textoEncriptado.value= textoEncriptar;
+    textoIngreso.value="";
+   
+}
+
+function desencriptar(stringEncriptada){
+
+    let matrizCodigo= [["e", "enter"], ["i","imes"], ["a", "ai",],["o", "ober"], ["u", "ufat"]];
+    stringEncriptada = stringEncriptada.toLowerCase();
+
+    let i= 0
+
+    for (i=0; i < matrizCodigo.length; i++){ 
+
+        if(stringEncriptada.includes(matrizCodigo[i][1])){
+
+            stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0]);
+        }
+    }
+    return stringEncriptada;
+}
