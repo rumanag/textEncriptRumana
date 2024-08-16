33e5a9a880b0e2b4a263bef93dbb0e9d1343de78
Ricardo Umana <u3dide@gmail.com>
2024-08-16 16:20:37 -0500

    Terminada la version1 del programa con las funcionalidades básicas

--- textEncript.js
+++ textEncript.js
@@ -10,15 +10,30 @@ const textoEncriptado = document.querySelector(" .texto-encriptado");
 */
 
 function btnEncriptar(){
+    const textoValidar = document.querySelector( " .texto-ingreso");
+   
+    // validacion de los caracteres del texto
 
-    const textoEncriptar = encriptar(textoIngreso.value); //textoIngreso pasa el contenido a textoEncriptar.
-    textoEncriptado.value = textoEncriptar;
-    textoIngreso.value="";
-    textoEncriptado.style.backgroundImage= "none";
+    regex = /^[a-z]+$/;
+    
+    if ( regex.test(textoValidar.value)){  
+        const textoEncriptar = encriptar(textoIngreso.value); //textoIngreso pasa el contenido a textoEncriptar.
+        textoEncriptado.value = textoEncriptar;
+        textoIngreso.value="";
+        textoEncriptado.style.backgroundImage= "none";
+
+        var h3Oculto = document.querySelector(".informacion1 ").style.visibility="hidden";
+        var pOculto = document.querySelector(".headH6 ").style.visibility="hidden"; 
+    }   
+    else{
+         alert("El texto contiene caracteres no aceptados");
+        }  
 }
 
 function encriptar(stringEncriptada) {                                              // StringEncriptada tiene el contenido de textAra 
     
+    // valida los datos ingresados
+
     let matrizCodigo= [["e", "enter"], ["i","imes"], ["a", "ai",],["o", "ober"], ["u", "ufat"]];
         stringEncriptada = stringEncriptada.toLowerCase();
 
@@ -31,16 +46,29 @@ function encriptar(stringEncriptada) {                                          
             stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1]);  // ENCRIPTACIÓN: Reemplaza en el texto todos los
         }                                                                                           //  caracteres que se encuentran en el índice i, posición 0,
     }    
-    console.log(`stringEncriptada: ${stringEncriptada}`);                                                                                        // por los caracteres que se encuentran en el índice i, posición 1.
+                                                                                       // por los caracteres que se encuentran en el índice i, posición 1.
     return stringEncriptada;                                                        // pasa el valor de retorno a la llamada del  botonEncriptar 
 }
 
 function btnDesencriptar(){
 
-    const textoEncriptar = desencriptar(textoIngreso.value);
-    textoEncriptado.value= textoEncriptar;
-    textoIngreso.value="";
+    const textoValidar = document.querySelector( " .texto-ingreso");
    
+    // validacion de los caracteres del texto
+
+    regex = /^[a-z]+$/;
+    
+    if ( regex.test(textoValidar.value)){ 
+        const textoEncriptar = desencriptar(textoIngreso.value); //textoIngreso pasa el contenido a textoEncriptar.
+        textoEncriptado.value = textoEncriptar;
+        textoIngreso.value="";
+        textoEncriptado.style.backgroundImage= "none";
+        var h3Oculto = document.querySelector(".informacion1 ").style.visibility="hidden";
+        var pOculto = document.querySelector(".headH6 ").style.visibility="hidden"; 
+    }   
+    else{
+         alert("El texto contiene caracteres no aceptados");
+        }  
 }
 
 function desencriptar(stringEncriptada){
@@ -48,28 +76,51 @@ function desencriptar(stringEncriptada){
     let matrizCodigo= [["e", "enter"], ["i","imes"], ["a", "ai",],["o", "ober"], ["u", "ufat"]];
     stringEncriptada = stringEncriptada.toLowerCase();
 
+    let findEncriptados = 0
     let i= 0
-
+   
     for (i=0; i < matrizCodigo.length; i++){ 
 
-        if(stringEncriptada.includes(matrizCodigo[i][1])){
-
+        if( stringEncriptada.includes(matrizCodigo[i][1])){                              // Revisa si hay caracteres  iguales al contenido en el índice i, posición 1, de la matríz.
+            
             stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0]);
-        }
+
+            findEncriptados=1 ;                                                         // Por lo menos hay un encriptado
+        }  
     }
+
+    if (findEncriptados == 0) {
+
+        alert("No se encontraron encriptados")
+
+    }
+
     return stringEncriptada;
 }
 
 function btnCopiar() {
 
     let stringEncriptada= document.querySelector(".texto-encriptado");
-    stringEncriptada.select();
-    stringEncriptada.setSelectionRange(0,99999);  // versiones móbiles
 
-    navigator.clipboard.writeText(stringEncriptada.value).then(function() {
-            alert("¡Texto copiado al clipboard!");
-    }).catch(function(error){
-        console.error(`Falló la copia: ${error}`)
-    })
+    if (stringEncriptada == "") {
+        
+        alert("no hay encriptación para copiar");
+        
+    }   else {
+            stringEncriptada.select();
+            stringEncriptada.setSelectionRange(0,99999);  // versiones móbiles
+        
+            navigator.clipboard.writeText(stringEncriptada.value).then(function() {
+                    alert("¡Texto copiado al clipboard!");
+                    textoEncriptado.value="";
+                    textoIngreso.value="";
+                    textoEncriptado.style.backgroundImage= " url('./assets/imagenBuscar.png')"
+                    var h3Oculto = document.querySelector(".informacion1 ").style.visibility="visible";
+                    var pOculto = document.querySelector(".headH6 ").style.visibility="visible"; 
+        
+            }).catch(function(error){
+                    alert(`Falló la copia: ${error}`)
+        })
+    }
 }
 
