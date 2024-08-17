1ab3fdde66e046c466fa020b5dbfc6a2e409c08b
Ricardo Umana <u3dide@gmail.com>
2024-08-17 12:26:21 -0500

    Realizando detalles de terminado:limpieza de campos y alertas

--- textEncript.js
+++ textEncript.js
@@ -19,14 +19,13 @@ function btnEncriptar(){
     if ( regex.test(textoValidar.value)){  
         const textoEncriptar = encriptar(textoIngreso.value); //textoIngreso pasa el contenido a textoEncriptar.
         textoEncriptado.value = textoEncriptar;
-        textoIngreso.value="";
-        textoEncriptado.style.backgroundImage= "none";
-
-        var h3Oculto = document.querySelector(".informacion1 ").style.visibility="hidden";
-        var pOculto = document.querySelector(".headH6 ").style.visibility="hidden"; 
     }   
     else{
-         alert("El texto contiene caracteres no aceptados");
+         alert("El texto contiene caracteres no aceptados o está en blanco");
+         textoEncriptado.style.backgroundImage= " url('./assets/imagenBuscar.png')";
+         var h3Oculto = document.querySelector(".informacion1 ").style.visibility="visible";
+         var pOculto = document.querySelector(".headH6 ").style.visibility="visible";   
+         textoEncriptado.value="";
         }  
 }
 
@@ -37,21 +36,44 @@ function encriptar(stringEncriptada) {                                          
     let matrizCodigo= [["e", "enter"], ["i","imes"], ["a", "ai",],["o", "ober"], ["u", "ufat"]];
         stringEncriptada = stringEncriptada.toLowerCase();
 
-    let i=0
+    let findEncriptados = 0;
+    let i=0;
+    
+    /*  Revisa si hay caracteres  iguales al contenido en el índice i, posición 0, de la matríz. 
+        ENCRIPTACIÓN: Reemplaza en el texto todos los caracteres que se encuentran en el índice i, posición 0,   
+                                              por los caracteres que se encuentran en el índice i, posición 1. 
+    */
+
     for(  i=0; i < matrizCodigo.length; i++){
           
-        if(stringEncriptada.includes(matrizCodigo[i][0])){                          // Revisa si hay caracteres  iguales al contenido en el índice i, posición 0, de la matríz.
+        if(stringEncriptada.includes(matrizCodigo[i][0])){                          
+            stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1]);  
+            findEncriptados = 1;                                                                      
+        }                                                                                            
+    } 
+    /* Si no hay caracteres para encriptar, retorna
+    */
+   
+    if (findEncriptados == 0) {
 
+        alert("No se encontraron caracteres para encriptar");
+        textoEncriptada.value="";
+        textoEncriptado.style.backgroundImage= " url('./assets/imagenBuscar.png')";
+        var h3Oculto = document.querySelector(".informacion1 ").style.visibility="visible";
+        var pOculto = document.querySelector(".headH6 ").style.visibility="visible";        
+   
+    }   else {
 
-            stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1]);  // ENCRIPTACIÓN: Reemplaza en el texto todos los
-        }                                                                                           //  caracteres que se encuentran en el índice i, posición 0,
-    }    
-                                                                                       // por los caracteres que se encuentran en el índice i, posición 1.
-    return stringEncriptada;                                                        // pasa el valor de retorno a la llamada del  botonEncriptar 
+            textoIngreso.value="";
+            textoEncriptado.style.backgroundImage= (" none");
+            var h3Oculto = document.querySelector(".informacion1 ").style.visibility="hidden";
+            var pOculto = document.querySelector(".headH6 ").style.visibility="hidden"; 
+    }
+       
+    return stringEncriptada;                                                                         // pasa el valor de retorno a la llamada del  botonEncriptar 
 }
 
 function btnDesencriptar(){
-
     const textoValidar = document.querySelector( " .texto-ingreso");
    
     // validacion de los caracteres del texto
@@ -61,51 +83,59 @@ function btnDesencriptar(){
     if ( regex.test(textoValidar.value)){ 
         const textoEncriptar = desencriptar(textoIngreso.value); //textoIngreso pasa el contenido a textoEncriptar.
         textoEncriptado.value = textoEncriptar;
-        textoIngreso.value="";
-        textoEncriptado.style.backgroundImage= "none";
-        var h3Oculto = document.querySelector(".informacion1 ").style.visibility="hidden";
-        var pOculto = document.querySelector(".headH6 ").style.visibility="hidden"; 
     }   
     else{
-         alert("El texto contiene caracteres no aceptados");
+         alert("El texto contiene caracteres no aceptados o está en blanco");
+         textoEncriptado.style.backgroundImage= " url('./assets/imagenBuscar.png')";
+         var h3Oculto = document.querySelector(".informacion1 ").style.visibility="visible";
+         var pOculto = document.querySelector(".headH6 ").style.visibility="visible";  
+         textoEncriptado.value="";
         }  
 }
 
-function desencriptar(stringEncriptada){
+function desencriptar(stringEncriptada) {
 
     let matrizCodigo= [["e", "enter"], ["i","imes"], ["a", "ai",],["o", "ober"], ["u", "ufat"]];
     stringEncriptada = stringEncriptada.toLowerCase();
 
-    let findEncriptados = 0
-    let i= 0
+    let findEncriptados = 0;
+    let i= 0;
    
-    for (i=0; i < matrizCodigo.length; i++){ 
+    for (i=0; i < matrizCodigo.length; i++) {
 
-        if( stringEncriptada.includes(matrizCodigo[i][1])){                              // Revisa si hay caracteres  iguales al contenido en el índice i, posición 1, de la matríz.
-            
+        if( stringEncriptada.includes(matrizCodigo[i][1])) {                              
             stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0]);
-
-            findEncriptados=1 ;                                                         // Por lo menos hay un encriptado
-        }  
+            findEncriptados =1 ;                                     
+        }   
     }
 
     if (findEncriptados == 0) {
 
-        alert("No se encontraron encriptados")
+            alert("No se encontraron caracteres encriptados");
+            stringEncriptada="";
+            textoEncriptado.style.backgroundImage= " url('./assets/imagenBuscar.png')";
+            var h3Oculto = document.querySelector(".informacion1 ").style.visibility="visible";
+            var pOculto = document.querySelector(".headH6 ").style.visibility="visible"; 
 
+        }   else {
+
+            textoIngreso.value ="";
+            textoEncriptado.style.backgroundImage =(" none");
+            var h3Oculto = document.querySelector(".informacion1 ").style.visibility="hidden";
+            var pOculto = document.querySelector(".headH6 ").style.visibility="hidden"; 
     }
-
     return stringEncriptada;
-}
+  
+} 
+
 
 function btnCopiar() {
 
     let stringEncriptada= document.querySelector(".texto-encriptado");
 
-    if (stringEncriptada == "") {
-        
+    if (stringEncriptada.value == "") {
+       
         alert("no hay encriptación para copiar");
-        
     }   else {
             stringEncriptada.select();
             stringEncriptada.setSelectionRange(0,99999);  // versiones móbiles
@@ -114,13 +144,12 @@ function btnCopiar() {
                     alert("¡Texto copiado al clipboard!");
                     textoEncriptado.value="";
                     textoIngreso.value="";
-                    textoEncriptado.style.backgroundImage= " url('./assets/imagenBuscar.png')"
+                    textoEncriptado.style.backgroundImage= " url('./assets/imagenBuscar.png')";
                     var h3Oculto = document.querySelector(".informacion1 ").style.visibility="visible";
                     var pOculto = document.querySelector(".headH6 ").style.visibility="visible"; 
         
             }).catch(function(error){
-                    alert(`Falló la copia: ${error}`)
+                    alert(`Falló la copia: ${error}`);
         })
     }
 }
-
